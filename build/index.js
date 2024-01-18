e99d3096be3e0cd6957511b76ec708343279232a
Renan Menezes <2003rcsm@gmail.com>
2024-01-18 17:00:29 -0300

    Funções

--- build/index.js
+++ build/index.js
@@ -1,8 +1,32 @@
 "use strict";
-function printaObjetos(pessoa) {
-    console.log(pessoa);
+let ligado = false;
+let nome = "Renan";
+let idade = 30;
+let altura = 1.9;
+let nulo = null;
+let indefinido = undefined;
+let retorno;
+let retornoView = false;
+let produto = {
+    name: "Renan",
+    cidade: "Sp",
+    idade: 20,
+};
+let meuProduto = {
+    nome: "Tênis",
+    preco: 45,
+    unidades: 5,
+};
+let dados = ["Renan", "Ana", "Adriana"];
+let infos = ["Renan", 235];
+let boleto = ["agua conta", 256.32, 25465435];
+let aniversario = new Date("2022-12-01 05:00");
+console.log(aniversario.toString());
+function addNumber(x, y) {
+    return x + y;
 }
-printaObjetos({
-    name: "bruce wayne",
-    vulgo: "batman",
-});
+function addToHello(name) {
+    return `Hello ${name}`;
+}
+let soma = addNumber(4, 7);
+console.log(addToHello("Renan"));
--- src/index.ts
+++ src/index.ts
@@ -62,3 +62,27 @@ let boleto:[string,number,number] = ["agua conta", 256.32, 25465435];
  */
 let aniversario:Date = new Date ("2022-12-01 05:00");
 console.log(aniversario.toString());
+
+//funções
+function addNumber(x: number, y: number): number {
+    return x + y;
+}
+
+function addToHello(name: string){
+    return `Hello ${name}`;
+}
+
+console.log(addToHello("Renan"));
+
+//funções Multi Tipos
+function callToPhone(phone: number | string):number | string {
+    return phone;
+}
+let soma: number = addNumber(4,7);
+
+console.log(callToPhone(123131313131));
+
+//funções Async
+async function getDataBase(id: number): Promise<string> {
+    return "Renan";
+}
